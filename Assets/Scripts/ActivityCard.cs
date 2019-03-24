2fb6263053952cbac89ed0426abaf00c15736895
Aslak Hauglid <aslak.hauglid@gmail.com>
2019-03-24 13:49:08 +0100

    began card classes

---
+++ Assets/Scripts/ActivityCard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivityCard : MonoBehaviour
+{
+    public string description;
+    public List<ActivityChoice> choices;
+    public int minimumTurn;
+    public List<string> featurePreRequisites;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
