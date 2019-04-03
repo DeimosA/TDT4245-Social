93fdd41443fb876c72eae549b5ecd16d7476af47
Aslak Hauglid <aslak.hauglid@gmail.com>
2019-04-03 19:33:34 +0200

    moved buttons to mainscene

---
+++ Assets/Scripts/ui/EndTurnButtonController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTurnButtonController : MonoBehaviour
+{
+    private PlayerController playerController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        try
+        {
+            playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+
+        }
+        catch (System.NullReferenceException){
+            Debug.Log("Player not found");
+        }
+    }
+
+    public void OnClick()
+    {
+        if(playerController == null)
+        {
+            playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+        }
+        playerController.EndTurn();
+    }
+}
